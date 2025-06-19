e66fc9bf10e3e036dbb9764f81ae1b536d0eccaf
Flex <davidtavk06@mail.ru>
2025-06-19 22:35:22 +0300

    чуть изменил

--- project_ARKANOID/FormSettings.Designer.cs
+++ project_ARKANOID/FormSettings.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.DifficultyBox = new System.Windows.Forms.ComboBox();
             this.buttonSave = new System.Windows.Forms.Button();
+            this.labelDiff = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // DifficultyBox
@@ -39,14 +40,14 @@
             "Легко",
             "Средне",
             "Тяжело"});
-            this.DifficultyBox.Location = new System.Drawing.Point(26, 27);
+            this.DifficultyBox.Location = new System.Drawing.Point(37, 69);
             this.DifficultyBox.Name = "DifficultyBox";
             this.DifficultyBox.Size = new System.Drawing.Size(135, 24);
             this.DifficultyBox.TabIndex = 0;
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(26, 71);
+            this.buttonSave.Location = new System.Drawing.Point(37, 279);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(112, 33);
             this.buttonSave.TabIndex = 1;
@@ -54,16 +55,27 @@
             this.buttonSave.UseVisualStyleBackColor = true;
             this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
+            // labelDiff
+            // 
+            this.labelDiff.AutoSize = true;
+            this.labelDiff.Location = new System.Drawing.Point(34, 41);
+            this.labelDiff.Name = "labelDiff";
+            this.labelDiff.Size = new System.Drawing.Size(78, 16);
+            this.labelDiff.TabIndex = 9;
+            this.labelDiff.Text = "Сложность";
+            // 
             // FormSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(361, 342);
+            this.Controls.Add(this.labelDiff);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.DifficultyBox);
             this.Name = "FormSettings";
             this.Text = "Настройки";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +83,6 @@
 
         public System.Windows.Forms.ComboBox DifficultyBox;
         private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelDiff;
     }
 }
